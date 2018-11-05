19a601df7ef919730a58c3b70eb97859613a1463
Anup-Deshpande94 <anupbd.rymec@gmail.com>
2018-11-05 13:03:28 +0530

    main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 cout<<" hello world"<<endl;
 cout<<" hello team city"<<endl;
 cout<<" Hello folks of Pthinks"<<endl;
-cout<<" watsup man nikki"<<endl;
+cout<<" watsup man"<<endl;
 
 return 0;
 }
